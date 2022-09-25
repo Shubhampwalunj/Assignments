e56f3e5083a24d82924ca9c2508f24785a8b17e5
Shubs <86958175+Shubhampwalunj@users.noreply.github.com>
2022-09-25 16:07:03 +0530

    Add files via upload

---
+++ TO print table.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x,i;
+	cout<<"Enter the number ";
+	cin>>x;
+	for (i=1; i<=10; i++){
+		int Table = i*x;
+		cout<<Table<<endl;
+		
+	}
+	return 0;
+}
