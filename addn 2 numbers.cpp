3c459e5895c1d5012480820e24598e80a44e73fb
Shubs <86958175+Shubhampwalunj@users.noreply.github.com>
2022-09-25 15:54:59 +0530

    Add files via upload

---
+++ addn 2 numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int x,y,sum;
+	cout<<"enter the first number";
+	cin>>x;
+	cout<<"enter the second umber";
+	cin>>y;
+	sum = x+y;
+	cout<<"The sum of two numbers is: "<<sum<<endl;
+	return 0;
+}
