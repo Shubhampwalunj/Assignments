313ba53844b1df0aed546ec2f47e775e2ec21714
Shubs <86958175+Shubhampwalunj@users.noreply.github.com>
2022-09-25 15:39:48 +0530

    Add files via upload

---
+++ even or odd.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout<<"Enter the any whole number :-";
+	cin>>a;
+	if (a%2==0){
+		cout<<"The enter number "<<a << " is a even umber "<<endl;
+		
+	}
+	else{
+		cout<<"The enter number  "<<a <<" is a odd umber "<<endl;
+		
+	}
+	return 0;
+
+}
